50484e60522326a7dcae4d83dcd3e8a0749feeee
bekineox <beki.neox@gmail.com>
2026-02-20 00:34:40 +0300

    create automated test for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,10 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
-if (!cart) {
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart'));
+  if (!cart) {
     cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
@@ -9,6 +14,7 @@ if (!cart) {
     quantity: 1,
     deliveryOptionId: '2'
   }];
+  }
 }
 
 
